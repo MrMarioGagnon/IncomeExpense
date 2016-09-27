162060d1bfb9791375e24ee14512e4fee785fb87
MarioGagnon <mr.mario.gagnon@gmail.com>
2016-09-27 06:35:22 -0400

    Ajout demande d'authorisation pour acceder sdcard

--- app/src/main/java/com/mg/incomeexpense/utility/UtilityActivity.java
+++ app/src/main/java/com/mg/incomeexpense/utility/UtilityActivity.java
@@ -1,8 +1,13 @@
 package com.mg.incomeexpense.utility;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.mg.incomeexpense.R;
 import com.mg.incomeexpense.core.ItemRepositorySynchronizerMessageBuilder;
@@ -20,12 +25,40 @@ import com.mg.incomeexpense.transaction.TransactionRepositorySynchronizer;
 public class UtilityActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = UtilityActivity.class.getSimpleName();
+    private static final int REQUEST_WRITE_STORAGE = 112;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.utility_activity);
+
+        boolean hasPermission = (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+        if (!hasPermission) {
+
+            this.requestPermissions(
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_STORAGE);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode)
+        {
+            case REQUEST_WRITE_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    //reload my activity with permission granted or use the features what required the permission
+                } else
+                {
+                    Toast.makeText(this.getParent(), "The app was not allowed to write to your storage. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
 
     }
 
